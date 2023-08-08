91903dfadb9f051d62ae3d3f3dfe51aeeea08d83
AndresEstebanGonzalez <andresandye@gmail.com>
2023-08-07 23:27:12 -0300

    Search Container style complete.

--- src/components/SideMenu/SearchContainer/SearchContainer.jsx
+++ src/components/SideMenu/SearchContainer/SearchContainer.jsx
@@ -1,9 +1,33 @@
-import styles from '../SearchContainer/SearchContainer.module.scss';
+import { useState } from "react";
+
+import styles from "../SearchContainer/SearchContainer.module.scss";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowLeft,
+  faBars,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function SearchContainer() {
-    return (
-        <>
-            <h1>SearchContainer</h1>
-        </>
-    )
+  const [iconClicked, setIconClicked] = useState("none");
+
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.searchBar}>
+        <FontAwesomeIcon
+          className={styles.magnifyingGlassIcon}
+          style={{ display: iconClicked }}
+          icon={faMagnifyingGlass}
+        />
+        <FontAwesomeIcon className={styles.arrowLeft} icon={faArrowLeft} />
+        <input
+          type="search"
+          placeholder="Search or start new chat"
+          className={styles.searchInput}
+        />
+      </div>
+      <FontAwesomeIcon className={styles.icon} icon={faBars} />
+    </div>
+  );
 }
--- src/components/SideMenu/SideMenuHeader/SideMenuHeader.jsx
+++ src/components/SideMenu/SideMenuHeader/SideMenuHeader.jsx
@@ -1,11 +1,31 @@
 import styles from "../SideMenuHeader/SideMenuHeader.module.scss";
 
+import ProfilePicture from "../../../assets/images/profile-picture.jpeg";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUsers,
+  faCircleNotch,
+  faMessage,
+  faEllipsisVertical,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function SideMenuHeader() {
   return (
-    <>
-      <h1>SideMenuHeader</h1>
-    </>
+    <div className={styles.mainContainer}>
+      <div className={styles.leftContainer}>
+        <img
+          src={ProfilePicture}
+          className={styles.profilePicture}
+          alt="Profile Picture"
+        />
+      </div>
+      <div className={styles.rightContainer}>
+        <FontAwesomeIcon className={styles.icon} icon={faUsers} />
+        <FontAwesomeIcon className={styles.icon} icon={faCircleNotch} />
+        <FontAwesomeIcon className={styles.icon} icon={faMessage} />
+        <FontAwesomeIcon className={styles.icon} icon={faEllipsisVertical} />
+      </div>
+    </div>
   );
 }
