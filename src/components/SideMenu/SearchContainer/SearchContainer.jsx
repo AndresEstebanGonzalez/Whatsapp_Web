6e1b12bca80dc86bf13ef52c13ac26f8775c886c
AndresEstebanGonzalez <andresandye@gmail.com>
2023-08-08 21:01:53 -0300

    Elements in Search Container deleted.

--- src/components/SideMenu/SearchContainer/SearchContainer.jsx
+++ src/components/SideMenu/SearchContainer/SearchContainer.jsx
@@ -10,14 +10,11 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function SearchContainer() {
-  const [iconClicked, setIconClicked] = useState("none");
-
   return (
     <div className={styles.mainContainer}>
       <div className={styles.searchBar}>
         <FontAwesomeIcon
           className={styles.magnifyingGlassIcon}
-          style={{ display: iconClicked }}
           icon={faMagnifyingGlass}
         />
         <FontAwesomeIcon className={styles.arrowLeft} icon={faArrowLeft} />
